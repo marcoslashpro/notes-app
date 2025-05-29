e34cf200b808ebc4391dfe87bfd5c5977df8254e
marcoslashpro <tambascomarco35@gmail.com>
2025-05-29 16:04:19 +0200

    removed files

--- debug.py
+++
@@ -1,7 +0,0 @@
-from website import create_app, db
-from website.models import Note
-
-app = create_app()
-with app.app_context():
-    notes = Note.query.filter_by(user_id=3).all()
-    print(notes)  # Should return a list of notes
